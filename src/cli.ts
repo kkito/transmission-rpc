332fc111c92da904c96595e86a4fb947d9efaaab
kkito <kkitokkito@gmail.com>
2019-02-09 13:32:24 +0800

    get torrent

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,28 @@
 #!/usr/bin/env node
 
+import { Transmission } from './transmission';
+
+// 有两个参数 ， host 和 port
+const host = process.argv[2];
+let port = process.argv[3];
+if (!port) {
+  port = '9091';
+}
+
+const transmission = new Transmission({ host, port: parseInt(port, 10) });
+
+async function main() {
+  const result = await transmission.getTorrents();
+  // tslint:disable-next-line:no-console
+  console.log(result);
+}
 // tslint:disable-next-line:no-console
 console.log('hello world2');
+
+// tslint:disable-next-line:no-console
+console.log(host);
+
+// tslint:disable-next-line:no-console
+console.log(port);
+
+main();
