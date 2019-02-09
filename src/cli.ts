d6082821c819e14271ebdef7b722c12903f3fd7b
kkito <kkitokkito@gmail.com>
2019-02-09 16:02:10 +0800

    list print format

--- src/cli.ts
+++ src/cli.ts
@@ -20,8 +20,12 @@ const transmission = new Transmission({ host, port: parseInt(port, 10) });
 async function main() {
   if (action === 'list') {
     const result = await transmission.getTorrents();
-    // tslint:disable-next-line:no-console
-    console.log(result);
+    for (const x of result) {
+      // tslint:disable-next-line:no-console
+      console.log(
+        `id: ${x.id} \t percent: ${x.percentDone} \t name: ${x.name}`
+      );
+    }
   } else if (action === 'start') {
     // tslint:disable-next-line:no-console
     console.log('TODO');
